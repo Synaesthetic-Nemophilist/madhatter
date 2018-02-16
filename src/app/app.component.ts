7002edf0e167ee2650f97b48a99477818216fc60
nomaterials <nomaterials@gmail.com>
2018-02-16 21:17:17 +0200

    implement listen component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 
+import {SpeechService} from "./speech.service";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(public speech: SpeechService) {
+  }
+
+
 }
